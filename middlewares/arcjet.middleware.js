941ee00ad8eb4d059afca02b5a11e0e75d3d3bd0
jahrulo <jahruloseray@gmail.com>
2025-05-04 16:44:26 +0000

    refactor: Improve error handling and response structure in arcjet middleware

---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,37 @@
+import arc from "../config/arcjet.config";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await arc.protect(req);
+
+    // if decision is denied
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.writeHead(429, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Too Many Requests" }));
+      } else if (decision.reason.isBot()) {
+        res.writeHead(403, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "No bots allowed" }));
+      } else {
+        res.writeHead(403, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Forbidden" }));
+      }
+    } else if (decision.results.some(isSpoofedBot)) {
+      // Arcjet Pro plan verifies the authenticity of common bots using IP data.
+      // Verification isn't always possible, so we recommend checking the decision
+      // separately.
+      // https://docs.arcjet.com/bot-protection/reference#bot-verification
+      res.writeHead(403, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Forbidden" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Hello World" }));
+    }
+  } catch (err) {
+    console.log(`Arcjet Middleware Error: ${err}`);
+    next(err);
+  }
+};
+
+export default arcjetMiddleware;
